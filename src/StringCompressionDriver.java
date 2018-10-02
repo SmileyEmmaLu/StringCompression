31fadc8c86be9b43abf2e88162aeebe19f98617e
Emma Lu <elu20@elu20.local>
2018-10-02 08:27:45 -0600

    first commit

---
+++ src/StringCompressionDriver.java
@@ -0,0 +1,11 @@
+package PACKAGE_NAME;/*
+Name:elu20
+LabName:StringCompressionDriver
+Lab Purpose: The goal of StringCompressionDriver is to create a program 
+Date: 
+Collaborations: None
+On My Honor: EL
+*/
+
+public class StringCompressionDriver {
+}
